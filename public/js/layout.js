26b5c0befffd5f4a3babf3931243eecc94b0b5b3
Tran Phuoc Loc <giongnhuntlf@gmail.com>
2021-05-02 22:19:59 +0700

    layout template

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,15 +1,12 @@
 /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
-
-for (i = 0; i < dropdown.length; i++) {
+let dropdown = document.getElementsByClassName("dropdown-btn")
+let dropdownDisplayList = [ "none", "block"]
+let dropdownDisplayTrigger = 0;
+for (let i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function() {
-  this.classList.toggle("active");
-  var dropdownContent = this.nextElementSibling;
-  if (dropdownContent.style.display === "block") {
-  dropdownContent.style.display = "none";
-  } else {
-  dropdownContent.style.display = "block";
-  }
+    this.classList.toggle("active")
+    var dropdownContent = this.nextElementSibling;
+    dropdownDisplayTrigger = (dropdownDisplayTrigger + 1) % 2
+    dropdownContent.style.display = dropdownDisplayList[dropdownDisplayTrigger]
   });
 }
