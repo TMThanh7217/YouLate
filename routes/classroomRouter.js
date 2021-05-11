e6c06eb193f534ac8fc3c24fad47986f92e17a15
TMThanh7217 <onthidaihocvatly12@gmail.com>
2021-05-11 22:11:45 +0700

    : D

--- routes/classroomRouter.js
+++ routes/classroomRouter.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var classroomController = require('../controllers/classroomController');
 
 router.get('/', (req, res) => {
     // do something here
-    res.render('classroom', {
-        pageTitle: 'Classrooms'
-    })
+    classroomController
+        .getAll()
+        .then(data => {
+            res.locals.classrooms = data;
+            res.render('classroom', {
+                pageTitle: 'Classrooms'
+            })
+        })
+        .catch(err => res.send("Error: " + err));
  })
 
 module.exports = router;
