1db4b2f82643abaa31ffe4ad06c6db8584d56318
TMThanh7217 <onthidaihocvatly12@gmail.com>
2021-05-12 20:05:01 +0700

    Change a whoopsie in findById

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -23,14 +23,13 @@ controller.findById = async (id) => {
         sql: 'SELECT * FROM  "Events" where id = :id',
         plain: false, // return all records if false, else return the 1st record
         raw: true,
-        replacement: {id: id},
         type: QueryTypes.SELECT
     }
 
     return await models.sequelize.query(option.sql, {
         plain: option.plain,
         raw: option.raw,
-        replacement,
+        replacement: {id: id},
         type: option.type
     });
 };
